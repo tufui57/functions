1abe78ed33f31a459dae152391ce4214eacf5dbd
DESKTOP-3U6270B\nomur <nommi038@student.otago.ac.nz>
2019-10-03 13:40:05 +1300

    the previous function did not work for swiss data projection

--- F04_convert_Points_to_raster.R
+++ F04_convert_Points_to_raster.R
@@ -1,9 +1,33 @@
+convert_dataframe_to_raster <- function(data, # dataframe
+                                        ref, # reference raster for coordinate system
+                                        val, # Add raster value. This argument should be character string
+                                        coordinateNames 
+){
+  
+  # Set coordinates
+  points <- data[, coordinateNames]
+  coordinates(points) <- data[, coordinateNames]
+  
+  # Put values in point object
+  points$value <- data[, val]
+  
+  proj4string(points) <- proj4string(ref)
+  
+  # create blank raster
+  r <- raster(ext=extent(ref), res = res(ref))
+  
+  # Project points. Original coordinate system of BIOCLIM variables is WGS84
+  sp_raster <- rasterize(points, ref, field="value", fun = mean)
+  
+  
+  return(sp_raster)
+}
 
 
-project_and_convert_occurrencePoints_to_raster <- function(data, # list object with occurrence data
+project_and_convert_dataframe_to_raster <- function(data, # dataframe
                                                ref, # reference raster for coordinate system
                                                val, # Add raster value. This argument should be character string
-                                               coordinateNames # 
+                                               coordinateNames 
 ){
     
   # Project coordinates from WGS84 to NZTM
@@ -28,18 +52,21 @@ project_and_convert_occurrencePoints_to_raster <- function(data, # list object w
     proj4string(points) <- proj4string(ref)
     
     # Project points. Original coordinate system of BIOCLIM variables is WGS84
-    sp_raster <- rasterize(points, ref, fun = mean)
+    sp_raster <- rasterize(points, ref, points$value, fun = mean)
   }
 
   return(sp_raster)
 }
 
+### If coordinates for cells in dataframe is unevenly gridded, i.e., the intervals between coordinates of roww are not consistent,
+### the data cannot make raster or make wrong raster.
+### The following function fixes the unevenly gridded data.
 
 unevenly_gridded_dataframe_to_raster <- function(data, # dataframe
                                                  val, # colum name of raster values
                                                  coordinateNames, # column names of coodinates
                                                  geocoodingSystem, # CRS
-                                                 resol # resolution of raster
+                                                 resol # resolution of raster. Find the right resolution from he intervals between cell coordinates.
 ){
 data.r <- as.matrix(data[, c(coordinateNames, val)])
 colnames(data.r) <- c('X', 'Y', 'Z')
